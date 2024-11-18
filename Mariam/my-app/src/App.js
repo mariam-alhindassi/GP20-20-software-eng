042ffc79636aec1c4c209e6b922bfea0ec5f24a5
“mariam-alhindassi” <Maryam_s.m@hotmail.com>
2024-11-18 20:05:46 +0400

    m

--- Mariam/my-app/src/App.js
+++ Mariam/my-app/src/App.js
@@ -1,14 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <p className='mariam'> This is Mariam web site </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +17,8 @@ function App() {
         >
           Learn React
         </a>
+        
+
       </header>
     </div>
   );
--- Mariam/my-app/src/index.js
+++ Mariam/my-app/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Form } from './forms';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Form />
   </React.StrictMode>
 );
 
@@ -15,3 +16,5 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default App;
