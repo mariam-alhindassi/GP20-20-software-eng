208452db5517e1b717402b5ebbce0f4f31046a59
“mariam-alhindassi” <Maryam_s.m@hotmail.com>
2024-12-04 13:36:30 +0400

    update

--- Mariam/my-app/src/components/ConditionalComp.jsx
+++ Mariam/my-app/src/components/ConditionalComp.jsx
@@ -4,7 +4,7 @@ import Welcome from "./Welcome";
 export default function ConditionalComp (){
     
    
-    const display = true;
+    const display = false;
     return display ? <Welcome /> : <Code />;
 
 }
