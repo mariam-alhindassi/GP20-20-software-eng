631a083e1bbcc762f0074ea9eac55e28cb73799d
“mariam-alhindassi” <Maryam_s.m@hotmail.com>
2024-12-16 14:54:13 +0400

    up

---
+++ Mariam/web_react_apps/web2/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Registration from './registration.';
+
+
+function App() {
+  return (
+    
+    function App() {
+      return (
+        <main className='App'>
+          <App />
+        </main>
+      )
+    }
+  );
+}
+
+export default App;
