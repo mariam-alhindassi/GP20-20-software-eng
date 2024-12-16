a5ceab6e7390e3d562a9d76ec1a7cc762d145464
“mariam-alhindassi” <Maryam_s.m@hotmail.com>
2024-12-16 18:19:23 +0400

    new

--- Mariam/web_react_apps/web2/src/App.js
+++ Mariam/web_react_apps/web2/src/App.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import './App.css';
-import Form from './Components/Form';
-
+//import './styles/style.css';  // For files inside src/styles
+//import Forms from './Components/Forms';
+import Home from './Components/Home';
+import { Button } from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 function App() {
   return (
-    
-    function App() {
-      return (
-        <div className='App'>
-            <Form />
+    <div className='App'>
+            <Home />
 
         </div>
-      )
-    }
+    
   );
 }
 
---
+++ Mariam/web_react_apps/web2/src/Components/Home.js
@@ -0,0 +1,16 @@
+
+import React from "react";
+
+export default function Home(){
+    return  (
+        <div>
+        <body>
+            <h1> Hellow Word </h1>
+            <button> submit </button>
+        </body>
+
+    </div>
+
+    );
+   
+}
